eb344f01b12af65141f01c0ce97a6b0bb5e7a9a2
ansnoussi <anis0snoussi@gmail.com>
2019-02-27 20:43:23 +0000

    files

---
+++ Open_File/CatFile.java
@@ -0,0 +1,32 @@
+import java.io.* ;
+import java.awt.* ;
+
+public class CatFile {
+
+public static void main(String args []){
+
+try{
+	String fileName = args[0] ; 
+	System.out.println("le nom du fichier est : " + fileName);
+	File file = new File(fileName);
+	FileInputStream in = new FileInputStream(file);
+	int size = (int) file.length();
+	byte data[] = new byte[size];
+	in.read(data);
+	String s = new String(data);
+	System.out.println("le contenur du fichier est: ");
+	System.out.println(s);
+
+
+}catch(ArrayIndexOutOfBoundsException e1){
+	System.out.println("il faur préciser le nom du fichier");
+}catch(FileNotFoundException e2){
+	System.out.println("fichier non trouvée");
+}catch(IOException e3){
+	System.out.println("erreur de chargement du fichier");
+}
+
+
+}
+	
+}
